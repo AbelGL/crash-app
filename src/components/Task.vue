bfa3e4ab9d7913d7d725ece949e308c3c82e03fd
Abel García Luis <abgarcia@air-institute.com>
2022-07-28 14:39:46 +0200

    Creando fake base de datos

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,8 @@
 <template>
-  <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
     <h3>
       {{task.text}}
       <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
@@ -13,22 +16,31 @@ export default {
   name: "Task",
   props: {
     task: Object
-  },
+  }
 };
 </script>
 
 <style scoped>
 .fas {
-  color: red;
+  color: rgb(199, 25, 25);
+  position: absolute;
+  z-index: 999;
+  right: 15px;
 }
 .task {
   background: #f4f4f4;
   margin: 5px;
   padding: 10px 20px;
   cursor: pointer;
+  position: relative;
 }
 .task.reminder {
   border-left: 5px solid green;
+}
+
+.task.reminder h3,
+.task.reminder p {
+  text-decoration: line-through;
 }
 .task h3 {
   display: flex;
