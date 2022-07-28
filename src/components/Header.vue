76f0f1c32ca82ced5e973fa18814b23ce494b383
Abel García Luis <abgarcia@air-institute.com>
2022-07-28 14:12:02 +0200

    First Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+    <Button
+      @btn-click="$emit('show-add-task')"
+      :text="showAddTask ? 'Close': 'Add Task'"
+      :color="showAddTask ? 'red': 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "@/components/Button.vue";
+export default {
+  name: "Header",
+  components: { Button },
+  props: {
+    title: String,
+    showAddTask: Boolean
+  }
+};
+</script>
+
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="(task, index) in tasks" :key="index">
+      <Task
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+export default {
+  name: "Tasks",
+  components: {
+    Task
+  },
+  props: {
+    tasks: Array
+  },
+ emits: ['delete-task', 'toggle-reminder'],
+};
+</script>
+
+<style>
+</style>
